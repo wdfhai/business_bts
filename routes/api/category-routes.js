7e2ec70940986268547bf8cfd5aa0b705a483de8
wdfhai <wd.fhai@outlook.com>
2021-03-09 23:10:49 -0600

    fixed all Category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
 const { Category, Product, ProductTag } = require('../../models');
 
-// The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   console.log(res);
   try {
     const categoryData = await Category.findAll({
@@ -21,8 +18,6 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ 
@@ -31,7 +26,7 @@ router.get('/:id', async (req, res) => {
       }],
     });
     if(!categoryData){
-      res.status(404).json({message: 'No category with that ID.'});
+      res.status(404).json({message: 'No category found with that ID.'});
       return;
     }
     res.status(200).json(categoryData);
@@ -42,19 +37,18 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new category
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
+    console.log(res)
   }
 });
 
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
   try {
-    const categoryData = await Category.update({
+    const categoryData = await Category.update(req.body, {
       where: {id: req.params.id}
     });
     res.status(200).json(categoryData);
@@ -64,14 +58,13 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
       where: {id: req.params.id}
     });
 
-    if (!locationData) {
-      res.status(404).json({ message: 'No location found with this id!' });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
 
